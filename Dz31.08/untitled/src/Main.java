57f4ec607527eb44c0bc274f57d63db6b55750ce
tati1129 <tatka.bogat@gmail.com>
2022-09-04 23:58:07 +0200

    add homework

---
+++ Dz31.08/untitled/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//Дано цклое число num. Необходимо написать программу, которая выводит на экран таблицу умножения num  до 10
+
+        int num = 5;
+        int mult = 10;
+        int i= 0;
+        while (i <= mult) {
+            System.out.println(num + " * " + i + " = " + num * i);
+            i=i+1;
+        }
+    }
+    }
