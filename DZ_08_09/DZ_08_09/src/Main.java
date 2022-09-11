0c025929e3ace1d9638967b6f7804f3c583f461f
tati1129 <tatka.bogat@gmail.com>
2022-09-11 16:13:08 +0200

    add Homework Task Java

---
+++ DZ_08_09/DZ_08_09/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+       String str="Java is a very popular language";
+       int startIndex;
+       int endIndex;
+    }
+}
--- Dz31.08/untitled/src/Main.java
+++ Dz31.08/untitled/src/Main.java
@@ -2,12 +2,17 @@ public class Main {
     public static void main(String[] args) {
 //Дано цклое число num. Необходимо написать программу, которая выводит на экран таблицу умножения num  до 10
 
-        int num = 5;
+      /*int num = 5;
         int mult = 10;
         int i= 0;
         while (i <= mult) {
             System.out.println(num + " * " + i + " = " + num * i);
             i=i+1;
-        }
+        }*/
+
+        int startIndex;
+        int endIndex;
+        String
+        char ch=str.charAt(index);
     }
     }
